d66c979ed34399ef22a76504f6d25a8f0f03e6a9
Akilis16 <gabrielsebastiao16072005@gmail.com>
2025-09-28 13:03:40 -0300

    Only LSP

--- src/main/java/br/com/mariojp/solid/lspaccounts/CheckingAccount.java
+++ src/main/java/br/com/mariojp/solid/lspaccounts/CheckingAccount.java
@@ -1,7 +1,7 @@
 package br.com.mariojp.solid.lspaccounts;
 
-public class CheckingAccount extends Account {
-    @Override
+public class CheckingAccount extends Account implements Withdrawable{ 
+    @Override 
     public void withdraw(double amount) {
         if(balance < amount) throw new IllegalArgumentException("Saldo insuficiente");
         super.withdraw(amount);
